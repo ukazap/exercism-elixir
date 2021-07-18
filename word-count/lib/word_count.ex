8692e19a30b54dee585fae417670701aae2b83c4
Ukaza Perdana <ukaza@hey.com>
2021-07-19 00:43:53 +0700

    Solve word-count

---
+++ word-count/lib/word_count.ex
@@ -0,0 +1,17 @@
+defmodule WordCount do
+  @doc """
+  Count the number of words in the sentence.
+
+  Words are compared case-insensitively.
+  """
+  @spec count(String.t()) :: map
+  def count(sentence) do
+    sentence
+      |> String.replace("_", " ")
+      |> String.downcase
+      |> String.split(~r/[^\w\-]/u, trim: true)
+      |> Enum.reduce(%{}, fn(word, acc) ->
+            Map.update(acc, word, 1, &(&1 + 1))
+          end)
+  end
+end

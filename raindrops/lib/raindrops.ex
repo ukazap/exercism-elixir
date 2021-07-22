f08093fa2731fbcf325986682b220c45b0381890
Ukaza Perdana <ukaza@hey.com>
2021-07-22 15:19:52 +0700

    Solve raindrops

---
+++ raindrops/lib/raindrops.ex
@@ -0,0 +1,21 @@
+defmodule Raindrops do
+  @doc """
+  Returns a string based on raindrop factors.
+
+  - If the number contains 3 as a prime factor, output 'Pling'.
+  - If the number contains 5 as a prime factor, output 'Plang'.
+  - If the number contains 7 as a prime factor, output 'Plong'.
+  - If the number does not contain 3, 5, or 7 as a prime factor,
+    just pass the number's digits straight through.
+  """
+  @spec convert(pos_integer) :: String.t()
+  def convert(number) do
+    sounds = [{3, "Pling"}, {5, "Plang"}, {7, "Plong"}]
+             |> Enum.filter(fn {factor, sound} -> rem(number, factor) == 0 end)
+
+    case sounds do
+      [] -> "#{number}"
+      sounds -> Enum.map_join(sounds, fn {_, sound} -> sound end)
+    end
+  end
+end
